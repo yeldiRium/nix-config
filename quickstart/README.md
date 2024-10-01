59e4a7651e6467059331196a0f267ffda411e705
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2024-10-01 18:50:07 +0200

    [quickstart] add quickstart directory for fresh installs

---
+++ quickstart/README.md
@@ -0,0 +1,9 @@
+# Quickstart
+
+1. `sudo su`
+3. Find your system disk device using `lsblk`
+4. Put it in the config file at the appropriate place
+5. Run `sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ./disko.nix --arg device '"/dev/<device here>"'`
+6. Run `cp -r . /mnt/etc/nixos && cd /mnt/etc/nixos`
+6. Run `nixos-generate-config --no-filesystems --show-hardware-config > /mnt/etc/nixos/hardware-configuration.nix`
+8. Run `nixos-install --root /mnt --flake /mnt/etc/nixos#default`
---
+++ quickstart/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Nixos config flake";
+     
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {nixpkgs, ...} @ inputs:
+  {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        inputs.disko.nixosModules.default
+        (import ./disko.nix { device = "/dev/<set this!>"; })
+
+        ./configuration.nix
+      ];
+    };
+  };
+}
