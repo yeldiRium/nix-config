7d004ba507645bfb39c7c57cf2561adf86f8d8b8
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2024-10-18 08:42:51 +0200

    [gpg] simplify config

--- home/yeldir/features/cli/gpg.nix
+++ home/yeldir/features/cli/gpg.nix
@@ -14,20 +14,13 @@
 
   home.packages = lib.optional config.gtk.enable pkgs.gcr;
 
-  programs = let
-    fixGpg =
-      /*
-      bash
-      */
-      ''
-        gpgconf --launch gpg-agent
-      '';
-  in {
+  programs = {
     # Start gpg-agent if it's not running or tunneled in
     # SSH does not start it automatically, so this is needed to avoid having to use a gpg command at startup
     # https://www.gnupg.org/faq/whats-new-in-2.1.html#autostart
-    bash.profileExtra = fixGpg;
-    zsh.loginExtra = fixGpg;
+    zsh.loginExtra = ''
+      gpgconf --launch gpg-agent
+    '';
 
     gpg = {
       enable = true;
@@ -43,30 +36,26 @@
     };
   };
 
-  systemd.user.services = {
-    # Link /run/user/$UID/gnupg to ~/.gnupg-sockets
-    # So that SSH config does not have to know the UID
-    link-gnupg-sockets = {
-      Unit = {
-        Description = "link gnupg sockets from /run to /home";
-      };
-      Service = {
-        Type = "oneshot";
-        ExecStart = "${pkgs.coreutils}/bin/ln -Tfs /run/user/%U/gnupg %h/.gnupg-sockets";
-        ExecStop = "${pkgs.coreutils}/bin/rm $HOME/.gnupg-sockets";
-        RemainAfterExit = true;
-      };
-      Install.WantedBy = ["default.target"];
-    };
-  };
+  # systemd.user.services = {
+  #   # Link /run/user/$UID/gnupg to ~/.gnupg-sockets
+  #   # So that SSH config does not have to know the UID
+  #   link-gnupg-sockets = {
+  #     Unit = {
+  #       Description = "link gnupg sockets from /run to /home";
+  #     };
+  #     Service = {
+  #       Type = "oneshot";
+  #       ExecStart = "${pkgs.coreutils}/bin/ln -Tfs /run/user/%U/gnupg %h/.gnupg-sockets";
+  #       ExecStop = "${pkgs.coreutils}/bin/rm $HOME/.gnupg-sockets";
+  #       RemainAfterExit = true;
+  #     };
+  #     Install.WantedBy = ["default.target"];
+  #   };
+  # };
 
-  home = {
-    persistence = {
-      "/persist/${config.home.homeDirectory}" = {
-        directories = [
-          ".gnupg"
-        ];
-      };
-    };
+  home.persistence."/persist/${config.home.homeDirectory}" = {
+    directories = [
+      ".gnupg"
+    ];
   };
 }
