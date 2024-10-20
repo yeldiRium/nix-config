90c13861b0f4411170d9dd88910ed3fe24ed96de
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2024-10-20 22:19:45 +0200

    [hledger] improve environment with vscode extensions and shell aliases

--- home/yeldir/features/desktop/office/hledger.nix
+++ home/yeldir/features/desktop/office/hledger.nix
@@ -1,4 +1,9 @@
-{pkgs, ...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   home = let
     hltool = name:
       pkgs.writeTextFile {
@@ -23,8 +28,33 @@
       (hltool "hlmonthend")
       (hltool "hlprojects")
     ];
+    shellAliases = {
+      "hl" = "hledger";
+      "hlc" = "hledger --strict check";
+    };
     sessionVariables = {
       LEDGER_FILE = "$HOME/querbeet/workspace/ledger/all.journal";
+    };
+  };
+
+  programs.vscode = {
+    extensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "ledger";
+        publisher = "mariosangiorgio";
+        version = "1.0.9";
+        sha256 = "sha256:1lyhldyl4f1c2hxqwn6arma8p1snm2cbrdr1vybxllrh570lcgz9";
+      }
+    ];
+    userSettings = lib.optionalAttrs config.programs.vscode.enable {
+      "ledger.binary" = "${lib.getExe pkgs.hledger}";
+      "files.associations" = {
+        "*.journal" = "ledger";
+        "*.prices" = "ledger";
+      };
+      "[ledger]" = {
+        "editor.rulers" = [54];
+      };
     };
   };
 }
