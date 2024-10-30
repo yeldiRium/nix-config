d53fd586a283814de32bd61a38bf702c08320095
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2024-10-30 22:16:48 +0100

    [neovim] fix undotree persistence

--- home/yeldir/features/cli/neovim/plugins/undotree.nix
+++ home/yeldir/features/cli/neovim/plugins/undotree.nix
@@ -1,5 +1,5 @@
 {config, pkgs, ...}: let 
-    undodir = "${config.home.homeDirectory}/.local/nvim/undodir";
+    undodir = ".local/share/nvim/undodir";
 in {
     programs.neovim.plugins = with pkgs.vimPlugins; [
         {
@@ -12,7 +12,7 @@ in {
 
                 vim.opt.swapfile = false
                 vim.opt.backup = false
-                vim.opt.undodir = "${undodir}"
+                vim.opt.undodir = "${config.home.homeDirectory}/${undodir}"
                 vim.opt.undofile = true
             '';
         }
