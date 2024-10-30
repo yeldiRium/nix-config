1000adf169d2b5edc96f1cbe50e064346ae53540
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2024-10-30 20:30:25 +0100

    [neovim] add keybind to clear search highlight

--- home/yeldir/features/cli/neovim/bindings.lua
+++ home/yeldir/features/cli/neovim/bindings.lua
@@ -3,3 +3,6 @@ vim.g.mapleader = " "
 -- Move through history
 vim.keymap.set("n", "<A-Left>", "<C-O>")
 vim.keymap.set("n", "<A-Right>", "<C-I>")
+
+-- Clear search highlight
+vim.keymap.set("n", "<leader>/", ":nohlsearch<cr>")
