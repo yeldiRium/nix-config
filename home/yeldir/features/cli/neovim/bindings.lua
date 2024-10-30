c88cfc9200319d8a7d4f8825db62e882da4f89b4
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2024-10-30 22:08:51 +0100

    [neovim] add keybindings to move between windows

--- home/yeldir/features/cli/neovim/bindings.lua
+++ home/yeldir/features/cli/neovim/bindings.lua
@@ -1,5 +1,11 @@
 vim.g.mapleader = " "
 
+-- Move between windows
+vim.keymap.set("n", "<S-A-Left>", "<C-w><Left>")
+vim.keymap.set("n", "<S-A-Right>", "<C-w><Right>")
+vim.keymap.set("n", "<S-A-Up>", "<C-w><Up>")
+vim.keymap.set("n", "<S-A-Down>", "<C-w><Down>")
+
 -- Move through history
 vim.keymap.set("n", "<A-Left>", "<C-O>")
 vim.keymap.set("n", "<A-Right>", "<C-I>")
