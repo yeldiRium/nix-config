56e42906eb9a2638d014e585f6e8f5bf4f45c3ab
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2024-10-30 21:54:08 +0100

    [neovim] make ranger quit after picking a file

--- home/yeldir/features/cli/neovim/plugins/rnvimr.nix
+++ home/yeldir/features/cli/neovim/plugins/rnvimr.nix
@@ -3,8 +3,16 @@
         {
             plugin = rnvimr;
             type = "lua";
-            config = ''
+            config = /* lua */ ''
                 vim.keymap.set("n", "<leader>pv", ":RnvimrToggle<cr>")
+
+                -- Replace netrw
+                vim.g["rnvimr_enable_ex"] = 1
+                -- Quit ranger popup after opening a file
+                vim.g["rnvimr_enable_picker"] = 1
+
+                -- Wipe the buffers corresponding to files deleted in ranger
+                vim.g["rnvimr_enable_bw"] = 1
             '';
         }
     ];
