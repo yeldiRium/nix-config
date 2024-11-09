41b6d81af0392e0b4a3f36ee7c758d6c7d5c7542
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2024-11-09 10:59:30 +0100

    [neovim] show current line number in addition to relative numbers

--- home/yeldir/modules/cli/neovim/options.lua
+++ home/yeldir/modules/cli/neovim/options.lua
@@ -1,3 +1,4 @@
+vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
@@ -9,6 +10,7 @@ vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
+-- Fixed width 3 sign column 
 vim.opt.signcolumn = "yes:3"
 
 vim.opt.ignorecase = true
