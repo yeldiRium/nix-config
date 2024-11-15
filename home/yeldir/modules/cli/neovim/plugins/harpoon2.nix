e5eb946333687d864dc7ed9c5475eee1a476563c
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2024-11-15 14:18:25 +0100

    [neovim] bind more harpoon keys

--- home/yeldir/modules/cli/neovim/plugins/harpoon2.nix
+++ home/yeldir/modules/cli/neovim/plugins/harpoon2.nix
@@ -35,6 +35,11 @@ in {
             vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
             vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
             vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
+            vim.keymap.set("n", "<C-5>", function() harpoon:list():select(5) end)
+            vim.keymap.set("n", "<C-6>", function() harpoon:list():select(6) end)
+            vim.keymap.set("n", "<C-7>", function() harpoon:list():select(7) end)
+            vim.keymap.set("n", "<C-8>", function() harpoon:list():select(8) end)
+            vim.keymap.set("n", "<C-9>", function() harpoon:list():select(5) end)
           '';
       }
     ];
