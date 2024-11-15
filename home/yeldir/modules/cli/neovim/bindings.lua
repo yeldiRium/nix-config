b61f6a5268a8b88451bc21c9e9e85d088ed24e32
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2024-11-15 14:56:47 +0100

    [neovim] add keybinding for lsp.buf.rename

--- home/yeldir/modules/cli/neovim/bindings.lua
+++ home/yeldir/modules/cli/neovim/bindings.lua
@@ -19,18 +19,20 @@ vim.keymap.set("n", "<A-O>", "O<Esc>")
 
 -- Programming
 -- LSP
-vim.keymap.set("n", "<leader>cr", "<cmd>LspRestart<cr>")
+vim.keymap.set("n", "<leader>cR", "<cmd>LspRestart<cr>")
 vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format)
 vim.keymap.set("n", "<leader>ch", vim.lsp.buf.hover, { desc = "Hover documentation" })
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
 vim.keymap.set("n", "<leader>ci", function()
   vim.lsp.buf.code_action({ source = { only = { "source.organizeImports" }}, apply = true })
 end, { desc = "Organize imports" })
--- Movement
+--   refactoring
+vim.keymap.set("n", "<leader>crr", vim.lsp.buf.rename, { desc = "Rename symbol" })
+--   navigation
 vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
 vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
 vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Go to references" })
--- Diagnostics
+--   diagnostics
 vim.keymap.set("n", "<leader>dd", vim.diagnostic.open_float, { desc = "Floating diagnostic" })
 vim.keymap.set("n", "gq", vim.diagnostic.setqflist, { desc = "Diagnostics on quickfix" })
