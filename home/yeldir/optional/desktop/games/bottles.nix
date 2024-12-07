ea26443356121c038765dba496464a0c7c40ab06
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2024-12-07 12:25:14 +0100

    [desktop] Make bottles work with new gtk renderer

--- home/yeldir/optional/desktop/games/bottles.nix
+++ home/yeldir/optional/desktop/games/bottles.nix
@@ -1,11 +1,25 @@
 {
   config,
+  lib,
   pkgs,
   ...
 }: {
   home.packages = with pkgs; [
     bottles
   ];
+
+  xdg.desktopEntries = {
+    "com.usebottles.bottles" = {
+      name = "Bottles";
+      type = "Application";
+      exec = "env GSK_RENDERER=ngl ${lib.getExe pkgs.bottles} %u";
+      icon = "com.usebottles.bottles";
+      categories = ["Utility" "GNOME" "GTK"];
+      mimeType = ["x-scheme-handler/bottles" "application/x-ms-dos-executable" "application/x-msi" "application/x-ms-shortcut" "application/x-wine-extension-msp"];
+      terminal = false;
+      startupNotify = true;
+    };
+  };
 
   home.persistence = {
     "/persist/${config.home.homeDirectory}" = {
