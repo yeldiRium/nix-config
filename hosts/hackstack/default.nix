3a17c5b732b0cb72ff31e53322c49412171ffe53
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2024-12-09 17:23:41 +0100

    [hackstack] Begin preparing hackstack configuration

---
+++ hosts/hackstack/default.nix
@@ -0,0 +1,62 @@
+{
+  pkgs,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+    (import ./disko.nix {device = "/dev/xxx-tbd";})
+
+    ../common/global
+
+    ../common/optional/bluetooth.nix
+    ../common/optional/greetd.nix
+    ../common/optional/networkmanager.nix
+    ../common/optional/pipewire.nix
+    # TODO: activate once the rest works ../common/optional/quietboot.nix
+
+    ../common/optional/mounts/datengrab.nix
+
+    ../common/users/yeldir
+  ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  environment.systemPackages = with pkgs; [
+  ];
+
+  networking = {
+    hostName = "hackstack";
+    hostId = "963c546b";
+  };
+
+  yeldirs = {
+    mounts = {
+      datengrab.enable = true;
+    };
+  };
+
+  programs = {
+    dconf.enable = true;
+  };
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.11"; # Did you read the comment?
+}
