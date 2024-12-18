9468215634feff5d000e4609c3df67a149f9693a
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2024-12-18 11:27:48 +0100

    [audio] Make pwvucontrol work with new renderer changes

--- home/yeldir/optional/desktop/common/volumecontrol.nix
+++ home/yeldir/optional/desktop/common/volumecontrol.nix
@@ -1,5 +1,17 @@
-{pkgs, ...}: {
+{lib, pkgs, ...}: {
   home.packages = with pkgs; [
     pwvucontrol
   ];
+
+  xdg.desktopEntries = {
+    "com.saivert.pwvucontrol" = {
+      name = "pwvucontrol";
+      type = "Application";
+      exec = "env GSK_RENDERER=ngl ${lib.getExe pkgs.pwvucontrol}";
+      icon = "com.saivert.pwvucontrol";
+      categories = ["Audio" "System"];
+      terminal = false;
+      startupNotify = true;
+    };
+  };
 }
