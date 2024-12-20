7cf88df6de533e060caab80058cb4803072bbf57
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2024-12-20 11:06:41 +0100

    [mount] Move datengrab from nfs to sshfs

--- hosts/common/optional/mounts/datengrab.nix
+++ hosts/common/optional/mounts/datengrab.nix
@@ -1,18 +1,25 @@
-{config, lib, ...}: let
+{config, lib, pkgs, ...}: let
   cfg = config.yeldirs.mounts.datengrab;
 in {
   options = {
     yeldirs.mounts.datengrab.enable = lib.mkEnableOption "Enable mount datengrab";
   };
   config = lib.mkIf cfg.enable {
-    boot.supportedFilesystems = ["nfs"];
-    services.rpcbind.enable = true;
+    environment.systemPackages = with pkgs; [
+      sshfs
+    ];
+
+    programs.ssh.extraConfig = ''
+      Host datengrab
+        User yeldir
+        IdentityFile ${config.users.users.yeldir.home}/.ssh/hleutloff
+      '';
 
     systemd.mounts = [
       {
-        type = "nfs";
+        type = "fuse.sshfs";
         mountConfig = {
-          Options = "noatime";
+          Options = "allow_other,default_permissions";
         };
         what = "datengrab:/mnt/raid";
         where = "/mnt/datengrab";
