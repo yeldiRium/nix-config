93f48a0dbc471511d06c3b1bc1a4da9b8d575e09
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2024-12-23 13:55:04 +0100

    [hackstack] Adjust monitors

--- home/yeldir/hackstack.nix
+++ home/yeldir/hackstack.nix
@@ -98,40 +98,33 @@
 
   monitors = [
     {
-      name = "LVDS-1";
-      width = 1600;
-      height = 900;
-      workspace = "9";
-      position = "1920x800";
-    }
-    {
-      name = "HDMI-A-1";
+      name = "eDP-1";
       width = 1920;
       height = 1080;
+      position = "1920x800";
       primary = true;
-      position = "0x0";
     }
   ];
   autostart = [
     {
       command = "${lib.getExe pkgs.telegram-desktop}";
       workspace = "1";
-      monitor = "DP-3";
+      monitor = "DP-7";
     }
     {
       command = "${lib.getExe pkgs.google-chrome}";
       workspace = "2";
-      monitor = "DP-3";
+      monitor = "DP-6";
     }
     {
       command = "${lib.getExe pkgs.obsidian}";
       workspace = "5";
-      monitor = "DP-3";
+      monitor = "DP-6";
     }
     {
       command = "${lib.getExe pkgs.thunderbird}";
       workspace = "7";
-      monitor = "DP-3";
+      monitor = "DP-6";
     }
   ];
 }
