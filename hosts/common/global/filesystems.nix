46d941ceacc55ca589f91086800c3038740f57cd
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2025-01-15 12:17:32 +0100

    [system] Support ntfs filesystem.

--- hosts/common/global/filesystems.nix
+++ hosts/common/global/filesystems.nix
@@ -1,5 +1,5 @@
 {
-  boot.supportedFilesystems = ["nfs" "zfs" "nfts"];
+  boot.supportedFilesystems = ["nfs" "zfs" "ntfs"];
   boot.zfs.forceImportRoot = false;
   boot.zfs.allowHibernation = true;
 }
