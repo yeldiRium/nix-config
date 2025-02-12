b23454d9ef178d07bb6d33555d58c9e2532afbdc
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2025-02-12 12:27:02 +0100

    [wsl] Make user nixos linger.

--- hosts/shared/linux/users/nixos/default.nix
+++ hosts/shared/linux/users/nixos/default.nix
@@ -5,6 +5,8 @@
 }: let
   ifTheyExist = groups: builtins.filter (group: builtins.hasAttr group config.users.groups) groups;
 in {
+  # This user is exclusively intended for use in WSL.
+
   users.mutableUsers = true;
   users.users.nixos = {
     isNormalUser = true;
@@ -17,6 +19,8 @@ in {
 
     home = "/home/nixos";
     createHome = true;
+
+    linger = true;
   };
 
   home-manager.users.nixos = import ../../../../../home/nixos/wsl.nix;
