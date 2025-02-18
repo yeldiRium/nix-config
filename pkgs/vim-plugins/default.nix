40fa5bbc1c2067647cf16af6786036381d5ca1f7
Hannes Leutloff <hannes.leutloff-extern@deutschebahn.com>
2025-02-18 13:51:04 +0100

    [home/cli/neovim] Update NeoGit manually to v 2025-01-21

--- pkgs/vim-plugins/default.nix
+++ pkgs/vim-plugins/default.nix
@@ -1,4 +1,5 @@
 {pkgs}:{
   fold-cycle-nvim = pkgs.callPackage ./fold-cycle-nvim {};
+  neogit = pkgs.callPackage ./neogit {};
   nvim-early-retirement = pkgs.callPackage ./nvim-early-retirement {};
 }
