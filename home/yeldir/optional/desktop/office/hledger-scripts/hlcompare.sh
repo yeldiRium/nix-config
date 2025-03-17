922ca0babd399cc413c4f5bbdc29fe80ded290f5
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2025-03-17 14:16:06 +0100

    [home/office/hledger] Allow overriding day of month in hlcompare script

--- home/yeldir/optional/desktop/office/hledger-scripts/hlcompare.sh
+++ home/yeldir/optional/desktop/office/hledger-scripts/hlcompare.sh
@@ -1,6 +1,35 @@
 #!/usr/bin/env zsh
 
+help() {
+  echo "Usage: hlcompare [-d|--day-of-month number]"
+  echo ""
+  echo "  -d|--day-of-month  Day of the month on which to compare the previous months"
+  echo "     --help          Show this help"
+}
+
 DAY_OF_MONTH=$(date --date '+1 days' '+%d')
+
+while [ $# -gt 0 ]; do
+  case $1 in
+    -d|--day-of-month)
+      DAY_OF_MONTH="${2}"
+      shift; shift;
+      ;;
+    --help)
+      help
+      exit 0
+      ;;
+    *)
+      echo "Unknown option ${1}"
+      echo ""
+      help
+      exit 1
+      ;;
+  esac
+done
+
+echo "day of month: ${DAY_OF_MONTH}"
+
 BEGIN_DATE=$(date --date '-6 months' '+%Y-%m')
 
 hledger balancesheet \
