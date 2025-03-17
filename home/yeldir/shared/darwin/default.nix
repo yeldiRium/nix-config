6bd011e2af05ba0a7f4ff0bca62aebb5e45045cd
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2025-03-17 14:16:06 +0100

    [home] Restructure; Move some defaults around; Remove/add some software

---
+++ home/yeldir/shared/darwin/default.nix
@@ -0,0 +1,15 @@
+{ inputs, ...}: {
+  imports = [
+    inputs.mac-app-util.homeManagerModules.default
+  ];
+
+  yeldirs = {
+    system = {
+      disable-impermanence = true;
+
+      platform = "darwin";
+
+      keyboardLayout = "de";
+    };
+  };
+}
