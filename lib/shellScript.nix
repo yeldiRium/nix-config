d19ce12db24209075da2cf3aa01979479387f443
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2025-03-17 14:16:22 +0100

    [lib] Add lib with a shellScript function

---
+++ lib/shellScript.nix
@@ -0,0 +1,13 @@
+pkgs: path: let
+  name = builtins.baseNameOf path;
+in
+  pkgs.writeTextFile {
+    inherit name;
+    executable = true;
+    destination = "/bin/${name}";
+    text = builtins.readFile path;
+    checkPhase = ''
+      ${pkgs.stdenv.shellDryRun} "$target"
+    '';
+    meta.mainProgram = name;
+  }
