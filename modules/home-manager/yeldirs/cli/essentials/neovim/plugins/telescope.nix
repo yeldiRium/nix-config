5e9e79ac5032b1fc4f96b9dd1f0e66c85cd66619
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2025-05-08 19:05:07 +0200

    [home/cli/neovim] Fix typo in binding description

--- modules/home-manager/yeldirs/cli/essentials/neovim/plugins/telescope.nix
+++ modules/home-manager/yeldirs/cli/essentials/neovim/plugins/telescope.nix
@@ -50,7 +50,7 @@ in {
             vim.keymap.set("n", "<leader>fm", telescope.marks, { desc = "Show marks" })
             vim.keymap.set("n", "<leader>fr", telescope.registers, { desc = "Show register contents" })
             vim.keymap.set("n", "<leader>fq", telescope.quickfix, { desc = "Show quickfixes" })
-            vim.keymap.set("n", "<leader>fd", telescope.diagnostics, { desc = "Show diagnosics" })
+            vim.keymap.set("n", "<leader>fd", telescope.diagnostics, { desc = "Show diagnostics" })
             vim.keymap.set("n", "<leader>fh", telescope.jumplist, { desc = "Show jumplist" })
 
             -- replaces the bindings for lsp related actions from ../bindings.lua
