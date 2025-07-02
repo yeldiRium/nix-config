f19ac7902824850bdcb537d50781d3c11b2553fd
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2025-07-02 16:43:56 +0200

    [rekorder] add some darwin UI settings

--- home/yeldir/rekorder.nix
+++ home/yeldir/rekorder.nix
@@ -35,6 +35,26 @@
     };
   };
 
+  targets.darwin = {
+    defaults = {
+      "com.apple.dock" = {
+        autohide = true;
+      };
+      "com.apple.menuextra.battery" = {
+        ShowPercent = "YES";
+      };
+      "com.apple.menuextra.clock" = {
+        IsAnalog = false;
+        Show24Hour = true;
+        ShowAMPM = false;
+        ShowDate = 1;
+        ShowDayOfMonth = true;
+        ShowDayOfWeek = true;
+        ShowSeconds = true;
+      };
+    };
+  };
+
   home = {
     stateVersion = "24.05";
   };
