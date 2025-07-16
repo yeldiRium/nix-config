b5b7afef47e5ba0d0ff282a9bd9a8b7b3c517d5a
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2025-07-16 18:01:37 +0200

    [home/cli/rss] Add disabled colorscheme for newsboat
    This needs to be rewritten, since newsboat only supports [256 colors](https://newsboat.org/releases/2.40/docs/newsboat.html#_colors).

--- modules/home-manager/yeldirs/cli/media/rss.nix
+++ modules/home-manager/yeldirs/cli/media/rss.nix
@@ -4,6 +4,7 @@
   ...
 }: let
   cfg = config.yeldirs.cli.media.rss;
+  # c = config.colorscheme.colors // config.colorscheme.harmonized;
 in {
   options = {
     yeldirs.cli.media.rss.enable = lib.mkEnableOption "rss";
@@ -13,6 +14,21 @@ in {
     programs.newsboat = {
       enable = true;
       autoReload = true;
+
+      # extraConfig = ''
+      #   color background          ${c.on_background}         ${c.background}
+      #   color listnormal          ${c.on_background}         ${c.background}
+      #   color listfocus           ${c.on_primary_container}  ${c.primary_container}   bold
+      #   color listnormal_unread   ${c.on_background}         ${c.background}          bold
+      #   color listfocus_unread    ${c.on_primary_container}  ${c.primary_container}   bold
+      #   color title               ${c.on_primary_container}  ${c.primary_container}   bold
+      #   color info                ${c.on_primary_container}  ${c.primary_container}   bold
+      #   color hint-key            ${c.on_primary_container}  ${c.primary_container}   bold
+      #   color hint-keys-delimiter ${c.on_primary_container}  ${c.primary_container}
+      #   color hint-separator      ${c.on_primary_container}  ${c.primary_container}   bold
+      #   color hint-description    ${c.on_primary_container}  ${c.primary_container}
+      #   color article             ${c.on_background}         ${c.background}
+      # '';
 
       urls = [
         {
