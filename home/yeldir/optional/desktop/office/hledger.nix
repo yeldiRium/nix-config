fc36a066e5899e1e3ce92e1185fac3863d1c81e5
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2025-08-03 19:19:46 +0200

    [home/office/hledger] Remove vscode support

--- home/yeldir/optional/desktop/office/hledger.nix
+++ home/yeldir/optional/desktop/office/hledger.nix
@@ -1,6 +1,4 @@
 {
-  config,
-  lib,
   pkgs,
   ...
 }: {
@@ -19,29 +17,6 @@
     };
     sessionVariables = {
       LEDGER_FILE = "$HOME/querbeet/workspace/ledger/current.journal";
-    };
-  };
-
-  programs.vscode = {
-    profiles.default = {
-      extensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-        {
-          name = "ledger";
-          publisher = "mariosangiorgio";
-          version = "1.0.9";
-          sha256 = "sha256:1lyhldyl4f1c2hxqwn6arma8p1snm2cbrdr1vybxllrh570lcgz9";
-        }
-      ];
-      userSettings = lib.optionalAttrs config.programs.vscode.enable {
-        "ledger.binary" = "${lib.getExe pkgs.hledger}";
-        "files.associations" = {
-          "*.journal" = "ledger";
-          "*.prices" = "ledger";
-        };
-        "[ledger]" = {
-          "editor.rulers" = [54];
-        };
-      };
     };
   };
 }
