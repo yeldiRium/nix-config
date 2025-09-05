de56509e41961706f7da4c602e9bd68abb238621
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2025-09-05 22:34:20 +0200

    [worker] Add more lib and improve worker-add to include k3s parameters

--- lib/y/workers.nix
+++ lib/y/workers.nix
@@ -1,10 +1,35 @@
 {lib, ...}: let
-  workers = lib.readFile ../../workers.json |> lib.strings.fromJSON;
-  each = f: lib.mapAttrsToList (_: workerCfg: f workerCfg) workers;
+  workers =
+    lib.readFile ../../workers.json
+    |> lib.strings.fromJSON
+    |> lib.mapAttrs (name: cfg: let
+      hostName = "worker-${cfg.shortname}";
+    in
+      cfg
+      // {
+        inherit hostName;
+        hostId = "000${cfg.shortname}";
+        tailnetName = "${hostName}.tail3a1cd4.ts.net";
+      });
+  workersList = lib.attrValues workers;
+  serverList = lib.filter (w: w.k3s.server) workersList;
+  each = f: lib.map (workerCfg: f workerCfg) workersList;
 in {
-  inherit workers each;
+  inherit
+    workers
+    workersList
+    serverList
+    each
+    ;
+
+  count = lib.length workersList;
+  serverCount = lib.length serverList;
 
   eachToAttrs = f: lib.listToAttrs (each f);
 
-  ips = lib.mapAttrsToList (worker: workerCfg: workerCfg.ipv6) workers;
+  ips = lib.map (workerCfg: workerCfg.ipv6) workersList;
+
+  k3s = {
+    primaryName = lib.filter (w: w.k3s.clusterInit) |> lib.head |> lib.getAttr "tailnetName";
+  };
 }
