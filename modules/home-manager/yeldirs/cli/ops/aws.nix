014fcd1da78d5c207e07280498af50ca03ca6c85
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2025-09-09 17:24:52 +0200

    [home/cli/aws] Allow configuration of aws-cli sso profiles

--- modules/home-manager/yeldirs/cli/ops/aws.nix
+++ modules/home-manager/yeldirs/cli/ops/aws.nix
@@ -6,18 +6,43 @@
 }:
 let
   cfg = config.yeldirs.cli.ops.aws;
+
+  defaultOutput = "json";
 in
 {
   options = {
     yeldirs.cli.ops.aws = {
       enable = lib.mkEnableOption "aws";
       region = lib.mkOption {
-        type = lib.types.string;
+        type = lib.types.str;
         default = "eu-central-1";
       };
 
       ecrIntegration = lib.mkEnableOption "docker integration for ecr";
       ssmTools = lib.mkEnableOption "aws-ssm-tools";
+
+      ssoConfigs =
+        with lib.types;
+        lib.mkOption {
+          description = "A set of sso configs that are used to log in to aws.";
+          example = {
+            cool-profile = {
+              accountId = "012345678910";
+              roleName = "MyAwsRole";
+              startUrl = "https://my-sso-provider.com/start";
+              registrationScopes = "sso:account:access";
+            };
+          };
+          default = {};
+          type = attrsOf (submodule {
+            options = {
+              accountId = lib.mkOption { type = str; };
+              roleName = lib.mkOption { type = str; };
+              startUrl = lib.mkOption { type = str; };
+              registrationScopes = lib.mkOption { type = str; };
+            };
+          });
+        };
     };
   };
 
@@ -58,8 +83,39 @@ in
         AWS_REGION = cfg.region;
       };
 
-      file = lib.mkIf cfg.ecrIntegration {
-        ".docker/config.json".text = "{\"credsStore\":\"ecr-login\"}";
+      file = {
+        ".docker/config.json" = {
+          enable = cfg.ecrIntegration;
+          text = "{\"credsStore\":\"ecr-login\"}";
+        };
+        ".aws/config" =
+          let
+            ssoConfigSections = lib.mapAttrsToList (
+              profileName: profileConfig: # toml
+              ''
+                [profile ${profileName}]
+                sso_session = ${profileName}
+                sso_account_id = ${profileConfig.accountId}
+                sso_role_name = ${profileConfig.roleName}
+                region = ${cfg.region}
+                output = ${defaultOutput}
+                [sso-session ${profileName}]
+                sso_start_url = ${profileConfig.startUrl}
+                sso_region = ${cfg.region}
+                sso_registration_scopes = ${profileConfig.registrationScopes}
+              ''
+            ) cfg.ssoConfigs;
+            ssoConfigLines = lib.concatLines ssoConfigSections;
+          in
+          {
+            text = # toml
+            ''
+              [default]
+              region=${cfg.region}
+              output=${defaultOutput}
+            ''
+            + ssoConfigLines;
+          };
       };
     };
 
