f53cc562e44dd1d16fd9dc341b192cc1e4a28e1c
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2025-09-18 15:09:19 +0200

    [pkgs] Clean up y packages a bit

--- pkgs/y/default.nix
+++ pkgs/y/default.nix
@@ -1,7 +1,7 @@
-{ pkgs, ... }@args:
+{ pkgs }:
 {
   konfig = pkgs.callPackage ./konfig.nix { };
   hledger-language-server = pkgs.callPackage ./hledger-language-server.nix { };
-  ijq = pkgs.callPackage ./ijq.nix args;
-  shellScript = pkgs.callPackage ./shellScript.nix args;
+  ijq = pkgs.callPackage ./ijq.nix { };
+  shellScript = pkgs.callPackage ./shellScript.nix { };
 }
