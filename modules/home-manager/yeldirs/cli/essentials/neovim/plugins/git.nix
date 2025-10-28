5ae03d7d51188ce820cf4dff2b01c1c964e34e24
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2025-10-28 10:51:13 +0100

    [home/cli/neovim] Remove neogit and dependencies

--- modules/home-manager/yeldirs/cli/essentials/neovim/plugins/git.nix
+++ modules/home-manager/yeldirs/cli/essentials/neovim/plugins/git.nix
@@ -13,43 +13,6 @@ in
   };
   config = lib.mkIf cfg.enable {
     programs.neovim.plugins = with pkgs.unstable.vimPlugins; [
-      nvim-web-devicons
-      {
-        plugin = diffview-nvim;
-        type = "lua";
-        config =
-          # lua
-          ''
-            require("diffview").setup({
-              keymaps = {
-                view = {
-                  { "n", "<C-c>", function() vim.cmd("DiffviewClose") end },
-                },
-                file_panel = {
-                  { "n", "<C-c>", function() vim.cmd("DiffviewClose") end },
-                },
-              },
-            })
-          '';
-      }
-      {
-        plugin = neogit;
-        type = "lua";
-        config =
-          # lua
-          ''
-            local neogit = require('neogit')
-            neogit.setup({
-              mappings = {
-                status = {
-                  ["<C-c>"] = "Close",
-                },
-              },
-            })
-
-            vim.keymap.set("n", "<leader>gg", "<cmd>Neogit<cr>", { desc = "Open NeoGit" })
-          '';
-      }
       {
         plugin = gitsigns-nvim;
         type = "lua";
