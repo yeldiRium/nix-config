faba985bd06b877c25d3e47bbc3dfbaef6887703
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2025-11-11 13:54:08 +0100

    [home/cli/devenv] Fix aliases

--- modules/home-manager/yeldirs/cli/essentials/devenv.nix
+++ modules/home-manager/yeldirs/cli/essentials/devenv.nix
@@ -18,8 +18,8 @@ in
         devs = "devenv shell zsh";
         devt = "devenv test";
         devtb = "devenv tasks run app:build";
-        devtl = "devenv task run app:lint";
-        devtlf = "devenv task run app:lint-fix";
+        devtl = "devenv tasks run app:lint";
+        devtlf = "devenv tasks run app:lint-fix";
       };
     };
   };
