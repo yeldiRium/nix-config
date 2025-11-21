2a2e48a95a9ace61cd799197885ec9708aab9dab
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2025-11-21 18:06:57 +0100

    [home/cli/neovim] Change more languages to 4-wide tabs

--- modules/home-manager/yeldirs/cli/essentials/neovim/ftplugin/go.lua
+++ modules/home-manager/yeldirs/cli/essentials/neovim/ftplugin/go.lua
@@ -1,6 +1,6 @@
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = false
 
 vim.opt.colorcolumn = "80,120"
