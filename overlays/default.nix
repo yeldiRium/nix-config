88ec461d5eb5cc9e902b0ea4e38d12c7a725e04e
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2026-01-02 09:41:49 +0100

    [overlays] Remove obsolete hy3 patches

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,15 +1,7 @@
 {
   inputs,
-  lib,
   ...
 }:
-let
-  addPatches =
-    pkg: patches:
-    pkg.overrideAttrs (oldAttrs: {
-      patches = (oldAttrs.patches or [ ]) ++ patches;
-    });
-in
 {
   additions =
     final: prev:
@@ -30,18 +22,6 @@ in
       inherit (final.stdenv.hostPlatform) system;
       config.allowUnfree = true;
     };
-  };
-
-  zz01-modifications = _: prev: {
-    unstable = lib.updateManyAttrsByPath [
-      {
-        path = [
-          "hyprlandPlugins"
-          "hy3"
-        ];
-        update = old: addPatches old [ ./hy3-0.43.0-disable-selection-hook.patch ];
-      }
-    ] prev.unstable;
   };
 
   # Remove this once neotest has a working version again.
