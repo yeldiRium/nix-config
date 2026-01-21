1964ec786c20d7b6d4059b814440e7d408914948
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2026-01-21 09:42:42 +0100

    [home/cli/neovim] Add missing dependency for grapple.nvim

--- modules/home-manager/yeldirs/cli/essentials/neovim/plugins/quickfilepicker.nix
+++ modules/home-manager/yeldirs/cli/essentials/neovim/plugins/quickfilepicker.nix
@@ -21,42 +21,55 @@ in
         message = "Can not activate neovim plugins harpoon2 and grapple at the same time";
       }
     ];
-    programs.neovim.plugins = with pkgs.unstable.vimPlugins; [
-      (lib.mkIf cfg.harpoon2.enable {
-        plugin = harpoon2;
-        type = "lua";
-        config =
-          # lua
-          ''
-            local harpoon = require('harpoon')
-            harpoon:setup({})
+    programs.neovim.plugins =
+      with pkgs.unstable.vimPlugins;
+      [
+        (lib.mkIf cfg.harpoon2.enable {
+          plugin = harpoon2;
+          type = "lua";
+          config =
+            # lua
+            ''
+              local harpoon = require('harpoon')
+              harpoon:setup({})
 
-            vim.keymap.set("n", "<leader>ma", function() harpoon:list():add() end, { desc = "Harpoon: Add current file to shortcuts" })
-            vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon: Show shortcuts" })
-            for i = 1, 9, 1 do
-              vim.keymap.set("n", "<M-"..i..">", function() harpoon:list():select(i) end, { desc = "Harpoon: Switch to shortcut "..i })
-              vim.keymap.set("n", "<M-k"..i..">", function() harpoon:list():select(i) end, { desc = "Harpoon: Switch to shortcut "..i })
-            end
-          '';
-      })
-      (lib.mkIf cfg.grapple.enable {
-        plugin = grapple-nvim;
-        type = "lua";
-        config =
-          # lua
-          ''
-            local grapple = require('grapple')
-            grapple.setup({})
+              vim.keymap.set("n", "<leader>ma", function() harpoon:list():add() end, { desc = "Harpoon: Add current file to shortcuts" })
+              vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon: Show shortcuts" })
+              for i = 1, 9, 1 do
+                vim.keymap.set("n", "<M-"..i..">", function() harpoon:list():select(i) end, { desc = "Harpoon: Switch to shortcut "..i })
+                vim.keymap.set("n", "<M-k"..i..">", function() harpoon:list():select(i) end, { desc = "Harpoon: Switch to shortcut "..i })
+              end
+            '';
+        })
+      ]
+      ++ (lib.optionals cfg.grapple.enable [
+        {
+          plugin = nvim-web-devicons;
+          type = "lua";
+          config =
+            # lua
+            ''
+              require("nvim-web-devicons").setup()
+            '';
+        }
+        {
+          plugin = grapple-nvim;
+          type = "lua";
+          config =
+            # lua
+            ''
+              local grapple = require('grapple')
+              grapple.setup({})
 
-            vim.keymap.set("n", "<leader>ma", grapple.toggle, { desc = "Grapple: Toggle tag on current file" })
-            vim.keymap.set("n", "<C-e>", grapple.toggle_tags, { desc = "Grapple: Show tags" })
-            for i = 1, 9, 1 do
-              vim.keymap.set("n", "<M-"..i..">", function() grapple.select({ index = i }) end, { desc = "Grapple: Switch to shortcut "..i })
-              vim.keymap.set("n", "<M-k"..i..">", function() grapple.select({ index = i }) end, { desc = "Grapple: Switch to shortcut "..i })
-            end
-          '';
-      })
-    ];
+              vim.keymap.set("n", "<leader>ma", grapple.toggle, { desc = "Grapple: Toggle tag on current file" })
+              vim.keymap.set("n", "<C-e>", grapple.toggle_tags, { desc = "Grapple: Show tags" })
+              for i = 1, 9, 1 do
+                vim.keymap.set("n", "<M-"..i..">", function() grapple.select({ index = i }) end, { desc = "Grapple: Switch to shortcut "..i })
+                vim.keymap.set("n", "<M-k"..i..">", function() grapple.select({ index = i }) end, { desc = "Grapple: Switch to shortcut "..i })
+              end
+            '';
+        }
+      ]);
 
     home.persistence = {
       "/persist" = {
