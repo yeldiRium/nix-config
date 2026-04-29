40bf66e0e1233a5c6c7353e39e3bba375634805f
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2026-04-29 08:14:06 +0200

    [pkgs] Update wayfinder-nvim to v0.1.7

--- pkgs/vim-plugins/wayfinder-nvim/default.nix
+++ pkgs/vim-plugins/wayfinder-nvim/default.nix
@@ -4,13 +4,13 @@
 }:
 vimUtils.buildVimPlugin rec {
   pname = "wayfinder-nvim";
-  version = "v0.1.6";
+  version = "v0.1.7";
   dontBuild = true;
   src = fetchFromGitHub {
     owner = "error311";
     repo = "wayfinder.nvim";
     rev = version;
-    sha256 = "sha256-nOfs0wwKH87XBMlBdhSsHk1atV8Ljg1aMgSVO8zfyC8=";
+    sha256 = "sha256-8JJmGF5dQ5zil/h1/xzCp8JxZUqKuw0fHEPOfh6duks=";
   };
   meta.homepage = "https://github.com/error311/wayfinder.nvim";
 }
