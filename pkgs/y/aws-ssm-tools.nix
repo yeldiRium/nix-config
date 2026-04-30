0054db36ee261892b2dabfa52793ef81f03ac0e5
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2026-04-30 09:58:49 +0200

    [pkgs] Update aws-ssm-tools to v2.2.1

--- pkgs/y/aws-ssm-tools.nix
+++ pkgs/y/aws-ssm-tools.nix
@@ -6,13 +6,13 @@
   with python3Packages;
   buildPythonPackage rec {
     name = "aws-ssm-tools";
-    version = "2.0.1";
+    version = "2.2.1";
     pyproject = true;
     src = fetchFromGitHub {
       owner = "mludvig";
       repo = "aws-ssm-tools";
       rev = "v${version}";
-      hash = "sha256-Pv4s0wnjrRKxdJxtQSIL+uTpkup2Civ2e9m6Dv/t2eM=";
+      hash = "sha256-RrbJ6s75DNX2bSGzHyRlEspdJl8g9eTjeCaoHxNXOCY=";
     };
     build-system = [
       hatchling
