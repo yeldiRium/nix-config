b907f94b45176df80317376bea967a59721a05b8
Hannes Leutloff <hannes.leutloff@yeldirium.de>
2026-09-24 18:00:50 +0200

    [home/cli/essentials] Improve lsblk alias
    Signed-off-by: Hannes Leutloff <hannes.leutloff@yeldirium.de>

--- modules/home-manager/yeldirs/cli/essentials/default.nix
+++ modules/home-manager/yeldirs/cli/essentials/default.nix
@@ -68,7 +68,7 @@ in
         batj = "bat --language json";
         baty = "bat --language yaml";
         hi = "ag --passthrough";
-        lsblk = "lsblk --output name,size,type,tran,fstype,owner,label";
+        lsblk = "lsblk --output name,size,type,tran,fstype,owner,label,mountpoint";
       };
     };
 
